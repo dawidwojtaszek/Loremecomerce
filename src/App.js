560b00456eb51e02f20a564a1904ff78f538d234
unknown <dawidw22@gmail.com>
2021-11-16 15:48:00 +0100

    fix: error with fetching data

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import GlobalStyle from './style/global';
 import Modal from './components/modal';
 import HeaderComponent from './components/header';
 import Footer from './components/footer';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { setItemsList } from './redux/items';
 import MobileMenu from './components/mobileMenu';
 import HomePage from './pages/homepage';
 import WishlistPage from './pages/wishlist';
 import ShopPage from './pages/shop';
 import { Routes, Route } from 'react-router-dom';
+import SHOP_DATA from './assets/data/shopData';
 
 
 
 function App() {
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setItemsList(SHOP_DATA));
+  }, [])
 
   const modalHidden = useSelector(state => state.modal.hidden);
   const menuHidden = useSelector(state => state.menu.hidden);
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import HeroSection from '../components/heroSection';
 import BenefitsSection from '../components/benefitsSection';
 import Bestsellers from "../components/bestsellers";
-import { useDispatch } from "react-redux";
 import SHOP_DATA from "../assets/data/shopData";
-import { setItemsList } from "../redux/items";
+
 
 const HomePage = () => {
 
-    const dispatch = useDispatch();
-    dispatch(setItemsList(SHOP_DATA));
     const bestsellers = SHOP_DATA.filter(item => (item.bestseller === true));
 
     return (
