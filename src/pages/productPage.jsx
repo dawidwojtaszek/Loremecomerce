bec3af185a85e6b2a03dc32c6c6a7ed235ff8b99
unknown <dawidw22@gmail.com>
2021-11-23 17:52:04 +0100

    fix: product page style

--- src/pages/productPage.jsx
+++ src/pages/productPage.jsx
@@ -3,7 +3,8 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router";
 import Product from '../style/productPage';
 import TabComponent from "../components/tab";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTag } from "@fortawesome/free-solid-svg-icons";
 const ProductPage = () => {
     const id = useParams();
     const product = useSelector(state => state.items.items).filter(item => (item.id === parseInt(id.id, 10)))[0];
@@ -14,7 +15,21 @@ const ProductPage = () => {
                 <Product.ImgBox>
                     <Product.Img src={product.imgUrl} />
                 </Product.ImgBox>
-                <Product.InfoBox>{product.name}</Product.InfoBox>
+                <Product.InfoBox>
+                    <Product.Name>
+                        {product.name}
+                    </Product.Name>
+                    <Product.Category>
+                        {product.category}
+                    </Product.Category>
+                    <Product.Description>
+                        {product.description}
+                    </Product.Description>
+                    <Product.Price>
+                        <FontAwesomeIcon icon={faTag} />
+                        ${product.price}
+                    </Product.Price>
+                </Product.InfoBox>
             </Product.Container>
             <TabComponent />
         </Product>
--- src/style/productPage/index.js
+++ src/style/productPage/index.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 const Product = styled.div`
     max-width: ${({ theme }) => theme.size.container};
     margin:auto;
+            -webkit-box-shadow: 7px 16px 25px 1px rgba(197, 197, 197, 1);
+    -moz-box-shadow: 7px 16px 25px 1px rgba(197, 197, 197, 1);
+    box-shadow: 7px 16px 25px 1px rgba(197, 197, 197, 1);
 `
 const Container = styled.div`
     display: flex;
@@ -10,27 +13,67 @@ const Container = styled.div`
     min-height: 600px;
     margin-top: 2rem;
     width: 100%;
-    background-color: violet;
-    
+
+    @media(max-width: 800px){
+        flex-direction: column;
+    }
 `
 const ImgBox = styled.div`
-    background-color: yellow;
+    padding: 1rem;
     height: 600px;
+    width: 500px;
+    @media(max-width: 800px){
+        width:80%;
+        height: auto;
+        align-self: center;
+    }
+
 `
 const Img = styled.img`
     height: 100%;
+    width: 100%;
+    object-fit: cover;
 `
 
 const InfoBox = styled.div`
-    background-color: green;
-    height: 100px;
-    width: 55%;
+    display: flex;
+    flex-direction: column;
+    margin-top: 1rem;
+    padding:1rem;
+    width: 700px;
+    @media(max-width:1200px){
+        width: 500px;
+    }
+    @media(max-width:1000px){
+        width: 400px;
+    }
 `
-
+const Name = styled.h3`
+    font-size: 1.8rem;
+    text-transform: uppercase;
+`
+const Category = styled.span`
+    font-size: 1.5rem;
+    text-transform: uppercase;
+    font-weight: 300;
+    color: ${({ theme }) => theme.colors.gray};
+`
+const Description = styled.p`
+    font-size: 1.3rem;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    padding-right: 2rem;
+`
+const Price = styled.span`
+    font-size: 2rem;
+`
 
 Product.Container = Container;
 Product.ImgBox = ImgBox;
 Product.Img = Img;
 Product.InfoBox = InfoBox;
-
+Product.Name = Name;
+Product.Category = Category;
+Product.Description = Description;
+Product.Price = Price;
 export default Product;
